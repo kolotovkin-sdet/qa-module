efc96fb3a179927296d7653aa44e52c2914269fc
Ilya Kolotovkin <skyraider565@gmail.com>
2025-06-29 19:46:25 +0300

    init

---
+++ src/test/java/com/qa/features/GrpcTests.java
@@ -0,0 +1,23 @@
+package com.qa.features;
+
+import com.qa.atlibs.steps.GrpcSteps;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GrpcTests {
+
+    @BeforeEach
+    void cleanUp() {
+
+    }
+
+    @Test
+    void demoTest_happyFlow() {
+//        GrpcSteps.prepareRequest()
+//                .jsonBodyForRequestIs()
+//                .metadataForRequestAre()
+//                .sendGrpcUnaryRequest()
+//                .jsonResponseIs();
+    }
+
+}
